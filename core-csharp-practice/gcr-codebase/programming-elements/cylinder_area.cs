316405f5d451c2cd5d753a86681d03755d5fd624
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 14:24:41 +0530

    [Prathmesh] Add. Finding the area of cylinder

---
+++ core-csharp-practice/gcr-codebase/programming-elements/cylinder_area.cs
@@ -0,0 +1,12 @@
+
+public class cylinder_area {
+	static void Main(){
+		//Enter radius and height
+		int radius = 10;
+		int height = 20;
+		//define with it's value
+		double pi = 3.14;
+		//calculating and printing the area
+		Console.WriteLine("Area for the given cylinder is : "+ pi*radius*radius*height );
+	}
+}
