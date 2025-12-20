9cbf452c1b3b14c1b0fe64cace8f692bf390cd72
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 12:50:30 +0530

    [Prathmesh] Add. Finding the area of circle

---
+++ core-csharp-practice/gcr-codebase/programming-elements/circle_area.cs
@@ -0,0 +1,11 @@
+public class circle_area {
+	static void Main(){
+		//Enter value for radius of the circle
+		int radius = 12;
+		//define variable pi
+		double pi = 3.14;
+		//calculating and printing the area
+		Console.WriteLine("Area for the given circle is : "+ pi*radius*radius );
+	}
+
+}
