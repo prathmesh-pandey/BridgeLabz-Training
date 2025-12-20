412780a478a80a8869dee1c55de9a2a21cb74762
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 19:22:24 +0530

    [Prathmesh] Add. Program for calculating average marks in PCM

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level-1/avg_Pcm_Marks.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class avgPcmMarks{
+	static void Main(){
+		int maths = 96;
+		int physics = 87;
+		int chemistry = 91;
+		int avgMarks = (maths + physics + chemistry)/3;
+		//Calculating and printing average marks in pcm
+		Console.WriteLine("Sam's average marks in PCM: " + avgMarks);
+	}
+}
