53cee300158654e800f601b4a9415b84321f23c6
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 19:21:33 +0530

    [Prathmesh] Add. Program for calculating age of Harry

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level-1/age_Of_Harry.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class ageOfHarry{
+	static void Main(){
+		//Enter Birthyear
+		int birthYear = 2004;
+		//Calculating and printing Harry's age
+		int harryAge = 2025 - birthYear;
+		Console.WriteLine("Current Age Of Harry: " + harryAge);
+	}
+}
