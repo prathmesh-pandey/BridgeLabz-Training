65a18e3abedaa20efdd8c90799e78fadcc3dda39
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 19:25:43 +0530

    [Prathmesh] Add. Program for converting distance from kms to miles

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level-1/distance_Conversion.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class distance_Conversion{
+	static void Main(){
+		double distance_In_Km = 10.8;
+		double distance_In_Miles = distance_In_Km*1.6;
+		Console.WriteLine("The Distance 10.8 km in Miles is " + distance_In_Miles);
+	}
+}
