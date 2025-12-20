599049d91c1e8e14474caeb6c05b4104663c5ef8
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 12:39:47 +0530

    [Prathmesh] Add program to calculate average of marks

---
+++ core-csharp-practice/gcr-codebase/programming-elements/average.cs
@@ -0,0 +1,13 @@
+
+public class average {
+	public static void Main(string[] args){
+		//Enter the numbers for calculating average
+		int num1 = 4;
+		int num2 = 5;
+		int num3 = 6;
+		//Calculating average
+		double average =  (num1+num2+num3)/3.0;
+		//Printing average
+		Console.WriteLine("Average = {0:F2}", average);
+	}
+}
