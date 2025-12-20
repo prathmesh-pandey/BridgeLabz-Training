27952c48b52bc7368381ea3a626ea0a773d83253
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 14:31:53 +0530

    [Prathmesh] Add. Changing the distance value from kms to miles

---
+++ core-csharp-practice/gcr-codebase/programming-elements/distance_conversion.cs
@@ -0,0 +1,10 @@
+public class distance_conversion {
+	public static void Main(string[] args){
+		//Enter the distance in kilometers
+		int Kilometers = 25;
+		//calculating and printing miles
+		double Miles = Kilometers*0.621371;
+		Console.WriteLine("Miles: "+ Miles);
+		
+	}
+}
