61bf48fead084cafd49201ef6d9fbcbfb23c49aa
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 15:52:44 +0530

    [Prathmesh] Add. Program for implementation of primitive data types and type conversion in csharp

---
+++ core-csharp-practice/gcr-codebase/programming-elements/DataType.cs
@@ -0,0 +1,74 @@
+public class DataTypes{
+	static void Main(){
+		//Primitive data types
+		
+		//byte data type 
+		byte byteValue = 5;
+		Console.WriteLine("Byte data type "+byteValue);
+		//typecasting byte to int
+		int byteInt = byteValue;
+		Console.WriteLine("Byte data type, type casted to int data type "+byteInt);
+		Console.WriteLine();
+		
+		//short data type
+		short Short = -165;
+		Console.WriteLine("Short data type "+Short);
+		//typecasting short to int
+		int shortInt = Short;
+		Console.WriteLine("Short data type, type casted to int data type "+shortInt);
+		Console.WriteLine();
+		
+		
+		//Integer data type
+		int Integer = 15;
+		Console.WriteLine("Integer data type "+Integer);
+	    //typecasting integer into float
+		float Integerf = Integer;
+		Console.WriteLine("Integer data type, type casted to float data type "+Integerf);
+		Console.WriteLine();
+		
+		
+		//Long data type
+		long Long = 987675434236788375;
+		Console.WriteLine("Long data type "+Long);
+		//typecasting Long to double
+		double Longd = (double)Long;
+		Console.WriteLine("Long data type, type casted to double data type "+Longd);
+		Console.WriteLine();
+		
+		
+		//Float data type
+		float Float = 11.73f;
+		Console.WriteLine("Float data type "+Float);
+		//typecasting float to double
+		double floatD = (double)Float;
+		Console.WriteLine("Float data type, type casted to double data type "+floatD);
+		Console.WriteLine();
+		
+		
+		//Double data type
+		double Double = 81896.09878586;
+		Console.WriteLine("Double data type "+Double);
+		//typecasting double to integer
+		int DoubleInt = (int)Double;
+		Console.WriteLine("Double data type, type casted to int data type "+DoubleInt);
+		Console.WriteLine();
+		
+		
+		//char data type
+		char Character = 'g';
+		Console.WriteLine("char data type "+Character);
+		//typecasting char to integer
+		int charInt = (int)Character;
+		Console.WriteLine("Char data type, type casted into int data type "+charInt);
+		Console.WriteLine();
+		
+		
+		//boolean data type
+		bool Boolean = false;
+		Console.WriteLine("Boolean data type "+Boolean);
+		
+		
+	}
+
+}
