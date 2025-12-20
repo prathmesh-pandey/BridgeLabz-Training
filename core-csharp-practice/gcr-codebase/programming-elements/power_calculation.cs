25d2da4fae440aff4a2360866b2d1b8153dd3389
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 14:56:34 +0530

    [Prathmesh] Add. Program to calculate power of a given base

---
+++ core-csharp-practice/gcr-codebase/programming-elements/power_calculation.cs
@@ -0,0 +1,10 @@
+public class power_calculation {
+	 static void Main(String[] args){
+		//Entert the base value and exponent value 
+		int basee = 4;
+		int exponent = 6;
+		//calculating and printing the result
+		Console.WriteLine("The result is : "+ Math.Pow(basee, exponent));
+	}
+
+}
