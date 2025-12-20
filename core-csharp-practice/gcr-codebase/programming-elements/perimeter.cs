603f3580d74948ddf8d9b7f4134ead04c39e0ac9
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 14:39:42 +0530

    [Prathmesh] Add. Finding the perimeter of rectangle

---
+++ core-csharp-practice/gcr-codebase/programming-elements/perimeter.cs
@@ -0,0 +1,9 @@
+public class perimeter {
+	static void Main(String[] args){
+		//Enter the value of length and width for calculating perimemter
+		int length = 5;
+		int width = 6;
+		//calculating and printing the perimeter of rectangle
+		Console.WriteLine("Perimeter of rectangle is : "+ 2*(length+width) );
+	}
+}
