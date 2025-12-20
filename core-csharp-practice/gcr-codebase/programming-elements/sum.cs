f4876fc9901b36aad5a1e0eeee947a3a9021c2eb
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 15:27:10 +0530

    [Prathmesh] Add. Program to calculate sum of two numbers

---
+++ core-csharp-practice/gcr-codebase/programming-elements/sum.cs
@@ -0,0 +1,11 @@
+public class sum {
+	static void Main(){
+		//Enter the numbers for sum
+		int num1 = 20;
+		int num2 = 30;
+		//calculate the sum
+		int sum = num1+num2;
+		//print the sum
+		Console.WriteLine("The sum is:"+sum);
+	}
+}
