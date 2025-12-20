a90af777a9b75708ecfdc37585c55e97c789ea6b
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 15:27:58 +0530

    [Prathmesh] Add. Program to convert the temperature from celsius to fahrenheit

---
+++ core-csharp-practice/gcr-codebase/programming-elements/temperature_conversion.cs
@@ -0,0 +1,12 @@
+public class temperature_conversion {
+	static void Main(){
+		//Enter value for temperature in celsius
+		int Celsius = 20;
+		//Converting to fahrenheit
+		int Fahrenheit = (Celsius * 9/5) + 32;
+		//Printing the result
+		Console.WriteLine("Temperature: "+Fahrenheit+" °F");
+		
+	}
+
+}
