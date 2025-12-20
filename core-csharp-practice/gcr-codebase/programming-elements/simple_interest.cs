2d86734b62fcb568007965acee6b9f177ba7b5c9
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-20 15:26:42 +0530

    [Prathmesh] Add. Program to calculate simple interest

---
+++ core-csharp-practice/gcr-codebase/programming-elements/simple_interest.cs
@@ -0,0 +1,10 @@
+public class simple_interest {
+	static void Main(){
+		//define principle, rate and time
+		int Principle = 1000;
+		int Rate = 10;
+		int Time = 5;
+		//calculating and printing Simple Interest 
+		Console.WriteLine("Simple Interest = "+ (Principle*Time*Rate)/100 );
+	}
+}
