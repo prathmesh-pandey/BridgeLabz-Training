95b1d5130dcee9c61d150b7e38ee600491d7d14a
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-22 16:27:31 +0530

    [Prathmesh] Add. Program to check divisibility by 5

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-1/CheckDivisibility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class CheckDivisibility
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number for checking divisibility");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 % 5 == 0)
+        {
+            Console.WriteLine("The number "+ num1+" is divisible by 5");
+        }
+        else
+        {
+            Console.WriteLine("The number "+num1 +" is not divisible by 5");
+        }
+    }
+}
