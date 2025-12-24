b68163cbc16dbce94b5917e7ae4359960bb0bd53
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:18:40 +0530

    [Prathmesh] Add. Program to calculate greatest factor of a number

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/GreatestFactor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class GreatestFactor
+{
+    static void Main(string[] args)
+    {
+
+        //Take input from user for calculating greatest factor
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+        int greatestFactor = 1;
+
+        //Looping from number-1 to 1
+        for (int i = number - 1; i >= 1; i--){
+            if (number % i == 0){
+                greatestFactor = i;
+                break;
+            }
+        }
+
+
+        //printing the greatest factor
+        Console.WriteLine("Greatest factor is: " + greatestFactor);
+    }
+}
