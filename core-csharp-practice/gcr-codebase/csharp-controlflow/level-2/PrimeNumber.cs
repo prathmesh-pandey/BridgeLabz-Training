2a771496a98285b83f9cf497e898e38882065be5
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:32:20 +0530

    [Prathmesh] Add. Program to evaluate input number is a prime number or not

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/PrimeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class PrimeNumber
+{
+    
+    static void Main(string[] args)
+    {
+        //Taking user input to input a number
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+
+        //Checking for numbers greater than 1
+        bool isPrime = true;
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+
+        else
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        //Printing result
+        if (isPrime)
+        {
+            Console.WriteLine(number + " is a Prime Number");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a Prime Number");
+        }
+    }
+}
