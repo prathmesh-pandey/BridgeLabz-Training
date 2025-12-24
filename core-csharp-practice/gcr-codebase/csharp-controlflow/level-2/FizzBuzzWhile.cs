13b18fd64f951ba498ac11514c0f503ff9a60b0c
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:17:27 +0530

    [Prathmesh] Add. Program to implement FizzBuzz program using while loop

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/FizzBuzzWhile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class FizzBuzzWhile
+{
+    static void Main(string[] args)
+    {
+        //Taking user input for endpoint
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+
+
+        //While loop for traversing
+        while (i <= number)
+        
+        {
+            //Conditions for checking where to print 
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+
+            i++;
+        }
+    }
+}
