bcb3a58eaf33579825740afe313bed46fbe888d6
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:15:40 +0530

    [Prathmesh] Add. Program to calculate factors of numbers

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/FactorsOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class FactorsOfNumber
+{
+    static void Main(string[] args)
+    {
+        //Taking user input for a number
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //Check factors of the numbers using FOR loop
+        for (int i = 1; i < number; i++){
+            if (number % i == 0){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
