8fcf2107575eed88edf072ab5f5f8a009a1a483a
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:14:45 +0530

    [Prathmesh] Add. Program to calculate BMI

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/BMI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class BMI
+{
+    static void Main(string[] args)
+    {
+        //Taking user input for height and weight
+        Console.WriteLine("Enter weight in kg: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter height in cm: ");
+        double heightCm = Convert.ToDouble(Console.ReadLine());
+
+        //Convert metrics 
+        double height = heightCm / 100;
+
+        //Calculating and printing result
+        double bmi = weight / (height * height);
+        Console.WriteLine("BMI value is: " + bmi);
+    }
+}
