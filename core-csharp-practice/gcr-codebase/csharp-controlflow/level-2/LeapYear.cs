47641e3241e1b081bb3e16cee71fadb000b8d8aa
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:24:37 +0530

    [Prathmesh] Add. Program to check which year is leapyear

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class LeapYear
+{
+    static void Main(string[] args)
+    {
+        //Taking user input to enter a year
+        Console.WriteLine("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            //Checking leap year conditions
+            if (year % 400 == 0)
+            {
+                Console.WriteLine(year + " is a Leap Year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(year + " is not a Leap Year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine(year + " is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a Leap Year");
+            }
+
+    }
+}
