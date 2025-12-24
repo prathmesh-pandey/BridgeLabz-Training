e816eaaebb8b319cf38e7011edecd507163ffd6a
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:30:33 +0530

    [Prathmesh] Add. Program to calculate power of a number

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/PowerOfNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class PowerOfNumber
+{
+    static void Main(string[] args)
+    {
+        //Taking user input for number and power
+        Console.WriteLine("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter power: ");
+        int power = Convert.ToInt32(Console.ReadLine());
+
+        int result = 1;
+
+
+        //For loop to calculate power
+        for (int i = 1; i <= power; i++){
+            result = result * number;
+        }
+
+
+        //printing the result
+        Console.WriteLine("Result is: " + result);
+    }
+}
