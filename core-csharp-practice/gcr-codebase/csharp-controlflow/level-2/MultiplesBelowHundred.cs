a1096544c3de510ee72603f98b201502da9602ae
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:29:43 +0530

    [Prathmesh] Add. Program to check to find all multiples of a the number input by user under 100

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/MultiplesBelowHundred.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class MultiplesBelowHundred
+{
+    static void Main(string[] args)
+    {
+        //Taking user input for a number
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+
+        //For Loop traversing backwards from 100 to 1
+        for (int i = 100; i >= 1; i--){
+            if (i % number == 0){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
