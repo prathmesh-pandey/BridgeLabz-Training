177c498daa25e3e16088a0a29b801cfe4fdf8065
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-12-24 11:17:54 +0530

    [Prathmesh] Add. Program to calculate grades of a student

---
+++ core-csharp-practice/gcr-codebase/csharp-controlflow/level-2/GradesCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class GradesCalculator
+{
+    static void Main(string[] args)
+    {
+
+        //Taking user input for marks
+        Console.WriteLine("Enter marks in Physics: ");
+        double physics = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter marks in Chemistry: ");
+        double chemistry = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter marks in Maths: ");
+        double maths = Convert.ToDouble(Console.ReadLine());
+
+
+        //Calculating average
+        double average = (physics + chemistry + maths) / 3;
+
+        //Printing grade
+        Console.WriteLine("Average Marks: " + average);
+
+        if (average >= 75)
+        {
+            Console.WriteLine("Grade: A, Remark: Excellent");
+        }
+        else if (average >= 60)
+        {
+            Console.WriteLine("Grade: B, Remark: Good");
+        }
+        else if (average >= 40)
+        {
+            Console.WriteLine("Grade: C, Remark: Pass");
+        }
+        else
+        {
+            Console.WriteLine("Grade: F, Remark: Fail");
+        }
+    }
+}
