af00f82a161e59b7dd1950b538aa7bc5d7be8711
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2026-01-06 12:53:45 +0530

    [Prathmesh] Add Program to display car rental details using constructor private methods

---
+++ oops-csharp-practice/gcr-codebase/c#-constructor/CarRentalSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class CarRental
+{
+
+    // Customer Rental details
+    private string customerName;
+    private string carModel;
+    private int rentalDays;
+    private double totalCost;
+
+
+    // Constructor
+    public CarRental(string customerName, string carModel, int rentalDays)
+    {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+
+
+        // Calculating cost
+        this.totalCost = CalculateCost();
+    }
+
+
+
+    // Private method
+    private double CalculateCost()
+    {
+        return rentalDays * 1500;
+    }
+
+
+    // Displaying rental info
+    public void Display()
+    {
+        Console.WriteLine($"{customerName} | {carModel} | ₹{totalCost}");
+    }
+
+    static void Main()
+    {
+        CarRental c = new CarRental("Aditi", "BMW M4 Competition", 3);
+        c.Display();
+    }
+}
