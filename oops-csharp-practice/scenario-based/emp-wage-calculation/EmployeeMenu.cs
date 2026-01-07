c4c71af13b3ade2d1bafc4c5f1f398fb833536e7
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2026-01-08 00:24:51 +0530

    [Prathmesh] Refactor. UC 4 Implemented to design a choice based menu for the program

--- oops-csharp-practice/scenario-based/emp-wage-calculation/EmployeeMenu.cs
+++ oops-csharp-practice/scenario-based/emp-wage-calculation/EmployeeMenu.cs
@@ -6,19 +6,17 @@ namespace Employee
     {
         private IEmployee employeeService;
 
-
-        public void Menu()
+        public void Menu() //UC 4 Used switch case to show menu 
         {
-
             employeeService = new EmployeeUtilityImpl();
-
 
             while (true)
             {
                 Console.WriteLine("1. Add Employee");
-                Console.WriteLine("2. Attendance Check");
-                Console.WriteLine("3. Display Employee");
-                Console.WriteLine("4. Exit");
+                Console.WriteLine("2. Attendance Check (UC1 + UC3)");
+                Console.WriteLine("3. Calculate Daily Wage using Switch Case (UC4)");
+                Console.WriteLine("4. Display Employee");
+                Console.WriteLine("5. Exit");
 
                 int choice = int.Parse(Console.ReadLine());
 
@@ -33,15 +31,21 @@ namespace Employee
                         break;
 
                     case 3:
-                        employeeService.DisplayEmployee();
+                        employeeService.CalculateDailyWage();
                         break;
 
                     case 4:
+                        employeeService.DisplayEmployee();
+                        break;
+
+                    case 5:
                         return;
-                
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
                 }
             }
-       
         }
     }
 }
