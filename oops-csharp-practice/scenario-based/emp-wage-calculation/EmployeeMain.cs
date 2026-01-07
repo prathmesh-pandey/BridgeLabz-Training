89630dd0019f777907704a3db187f7eaefffdffa
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2026-01-07 16:54:25 +0530

    [Prathmesh] Add Implemented UC 1 for employee wage calculation

---
+++ oops-csharp-practice/scenario-based/emp-wage-calculation/EmployeeMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employee
+{
+    internal class EmployeeMain
+    {
+        static void Main(string[] args)
+        {
+            EmployeeMenu menu = new EmployeeMenu();
+            menu.Menu();
+        }
+    }
+}
