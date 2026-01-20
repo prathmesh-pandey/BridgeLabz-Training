dbbaf0d822352817b9893a76f0e0efaa559216e0
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2026-01-20 15:50:00 +0530

    [Prathmesh] Add. Password cracking simulator to crack password using bruteforce and backtracking

---
+++ datastructures-csharp-practice/scenario-based/password-cracker/PasswordCracker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class PasswordCracker
+{
+    private string actualPassword;
+    private bool isCracked = false;
+
+    public PasswordCracker(string password)
+    {
+        actualPassword = password;
+    }
+
+    //Backtracking method
+    public void CrackPassword(char[] set, char[] current, int index)
+    {
+        //Stop if already found
+        if (isCracked)
+        {
+            return;
+        }
+
+        
+        //Base condition:password length reached
+        if (index == current.Length)
+        {
+            string attempt = new string(current);
+
+            Console.WriteLine("Trying: "+ attempt);
+
+            if (attempt == actualPassword)
+            {
+                Console.WriteLine("Password Cracked: " + attempt);
+                isCracked = true;
+            }
+            return;
+        }
+
+        //Try all characters for current position
+        for (int i = 0; i < set.Length; i++)
+        {
+            current[index] = set[i];
+            CrackPassword(set, current, index + 1);
+        }
+    }
+}
---
+++ datastructures-csharp-practice/scenario-based/password-cracker/PasswordMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class PasswordMain
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter password length:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter actual password:");
+        string password = Console.ReadLine().ToLower();
+
+        char[] characters = new char[26];
+
+        for (int i = 0; i < 26; i++)
+        {
+            characters[i] = (char)('a' + i);
+        }
+
+        char[] current = new char[n];
+
+        PasswordCracker cracker = new PasswordCracker(password);
+        cracker.CrackPassword(characters, current, 0);
+    }
+}
